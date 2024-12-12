fa9193f16054bf51df57ef9317421530d006c28a
Smartuil <563412673@qq.com>
2024-12-12 20:58:34 +0800

    升级项目到UE5.5.0版本

--- Source/WhatIsAnimationEditor.Target.cs
+++ Source/WhatIsAnimationEditor.Target.cs
@@ -8,8 +8,8 @@ public class WhatIsAnimationEditorTarget : TargetRules
 	public WhatIsAnimationEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
 		ExtraModuleNames.Add("WhatIsAnimation");
 	}
 }
